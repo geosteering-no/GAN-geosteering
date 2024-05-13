332667690bac000f682e3e2ac59980e729c9928e
Sergey Alyaev <saly@norceresearch.no>
2024-05-13 13:38:37 +0200

    Cosmetic changes

--- GAN/setup.py
+++ GAN/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
    name='GanLOG',
    version='1.0',
-   install_requires=["numpy","torch","tensorflow", "torchvision", "scikit-learn==0.22","scikit-image","seaborn"],
+   install_requires=["numpy", "torch", "tensorflow", "torchvision", "scikit-learn==0.22", "scikit-image", "seaborn"],
    py_modules=["GanLog"]
 )
