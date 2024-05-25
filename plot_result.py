f7b1ab813526c8c5a12e2683389559f6ffc579c2
Sergey Alyaev <saly@norceresearch.no>
2024-05-25 14:32:08 +0200

    Vizualization works, changing thicknesse, note debug

--- plot_result.py
+++ plot_result.py
@@ -48,7 +48,8 @@ def main():
     if not os.path.exists(plot_path):
         os.makedirs(plot_path)
 
-    path = [np.array([32,0])]
+    # todo load from the config file! @KriFos1
+    drilled_path = [np.array([32,0])]
     num_decission_points = 63
     # for i in range(num_decission_points):
     for i in range(1):
@@ -56,26 +57,44 @@ def main():
         checkpoint_at_step = np.load(f'estimate_decission_{i}.npz')
         state_vectors = checkpoint_at_step['m']
         position_at_step = checkpoint_at_step['pos']
+
+        # # todo remove test position
+        position_at_step = np.array([33, 4])
+        drilled_path.append(position_at_step)
+
         # this is the posterior
         post_earth = np.array(
             [create_weighted_image(evaluate_earth_model(gan_evaluator, state_vectors[:, el])) for el in
              range(ne)])  # range(state.shape[1])])
 
-        # next_position = pathfinder().run(, start_position)[0]
-        optimal_path = [perform_dynamic_programming(post_earth[j,:,:], position_at_step)[2] for j in range(ne)]
+        next_optimal, _garbage_path = pathfinder().run(state_vectors, position_at_step)
 
+        optimal_paths = [perform_dynamic_programming(post_earth[j,:,:], next_optimal)[2] for j in range(ne)]
+
+        # creating the figure
         fig, ax = plt.subplots(figsize=(10, 5))
-        norm = Normalize(vmin=-0.1, vmax=1)
-        ax.imshow(post_earth.mean(axis=0), cmap='tab20b', aspect='auto', norm=norm)
+
+        # visualizing the posterior
+        norm = Normalize(vmin=0.0, vmax=1)
+        im = ax.imshow(post_earth.mean(axis=0), cmap='tab20b', aspect='auto', norm=norm)
+        cbar = plt.colorbar(im, ax=ax)
+
+        # visualizing the optimal paths' remainders
+        earth_height = post_earth.shape[2]
+        max_height = earth_height - 0.5 - 0.01
+        min_height = 0.0 - 0.5 + 0.01
         for j in range(ne):
-            path_rows, path_cols = zip(*(path+optimal_path[j]))
-            ax.plot(path_cols, tuple([el + 0.2*np.random.randn() if c > len(path) else el for c,el in enumerate(path_rows)]),
-                    'k-', linewidth=0.5)
+            path_rows, path_cols = zip(*(drilled_path+optimal_paths[j]))
+            row_list = [el + 0.2*np.random.randn() if c > len(drilled_path) else el for (c, el) in enumerate(path_rows)]
+            row_list_truncated = [el if el < max_height else max_height for el in row_list]
+            row_list_truncated = [el if el > min_height else min_height for el in row_list_truncated]
+            ax.plot(path_cols, tuple(row_list_truncated),
+                    'k-', linewidth=0.2)
         ax.set_title('Result with Optimal Path', fontsize=18)
         plt.tight_layout()
         # plt.savefig(f'{plot_path}mean_earth_{i}.png')
 
-        path.append(checkpoint_at_step['pos'])
+        drilled_path.append(checkpoint_at_step['pos'])# note that we compute another one during viosualization
 
         plt.show()
 
